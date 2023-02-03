7086946b273dc5cbed73cf25b37091deb3305722
Myco <kmichael831@gmail.com>
2023-02-03 01:37:39 -0800

    edits

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const body = document.body
 const div = document.createElement("div")
 div.innerText = "what's coming is great"
 body.append(div)
+
+
+fetch('https://api.publicapis.org/entries')
+  .then((response) => response.json())
+  .then((data) => console.log(data));
