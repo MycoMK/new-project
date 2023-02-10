08323bcc11d68fda1f8af8d6acd80a6445fc774b
MICHAEL KINGS <57077025+MycoMK@users.noreply.github.com>
2023-02-09 19:09:44 -0800

    edits

--- script.js
+++ script.js
@@ -8,10 +8,27 @@
 //   .then((response) => response.json())
 //   .then((data) => console.log(data));
 
-let color = [ "#222f3e", "#f368e0","#ee5253", "#0abde3", "#0ac84","#222f3e","#5f27cd"];
-let i = 0;
- document.querySelector("button").addEventListener("click",
- function(){
-i = i < color.length ? ++ i : 0;
-let body = document.querySelector("body").style.background = "color"[i];
- })
+// let color = [ "#222f3e", "#f368e0","#ee5253", "#0abde3", "#0ac84","#222f3e","#5f27cd"];
+// let i = 0;
+//  document.querySelector("button").addEventListener("click",
+//  function(){
+// i = i < color.length ? ++ i : 0;
+// let body = document.querySelector("body").style.background = "color"[i];
+//  })
+
+
+let btn1 = document.querySelector("#btn-1");
+let btn2 = document.querySelector("#btn-2");
+let btn3 = document.querySelector("#btn-3");
+
+btn1.addEventListener("click", () =>{
+    document.body.style.backgroundImage = "cakes-3.jpg"
+});
+
+btn1.addEventListener("click", () =>{
+    document.body.style.backgroundImage = "fly bird.jpg"
+});
+
+btn1.addEventListener("click", () =>{
+    document.body.style.backgroundImage = "snow math.jpeg"
+});
