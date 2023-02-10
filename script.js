80ea4fd1659e3d155b6264a6f999a9b9c893980e
MICHAEL KINGS <57077025+MycoMK@users.noreply.github.com>
2023-02-09 18:31:50 -0800

    eidts-colors

--- script.js
+++ script.js
@@ -8,4 +8,10 @@
 //   .then((response) => response.json())
 //   .then((data) => console.log(data));
 
-
+let color = [ "#222f3e", "#f368e0","#ee5253", "#0abde3", "#0ac84","#222f3e","#5f27cd"];
+let i = 0;
+ document.querySelector("button").addEventListener("click",
+ function(){
+i = i < color.length ? ++ i : 0;
+let body = document.querySelector("body").style.background = "color"[i];
+ })
