9717b627607761e4c20e23f5007bb7e6e649993b
Myco <kmichael831@gmail.com>
2023-02-09 17:08:47 -0800

    edits

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const body = document.body
-const div = document.createElement("div")
-div.innerText = "what's coming is great"
-body.append(div)
+// const body = document.body
+// const div = document.createElement("div")
+// div.innerText = "what's coming is great"
+// body.append(div)
 
 
-fetch('https://api.publicapis.org/entries')
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+// fetch('https://api.publicapis.org/entries')
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+
+
